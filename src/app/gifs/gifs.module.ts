07a5ab33a4ab2712c1a961f34bd2c75ded4194d6
Juan Diego Ontiveros <ukf117@gmail.com>
2023-10-04 21:53:33 -0700

    Cambios viejos

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
